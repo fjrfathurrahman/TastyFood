815c141bd8fbddcbc34112dce3b043207d2aa845
Fajar Fathurrahman <jeynihgenk25@gmail.com>
2024-09-24 11:22:15 +0700

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Footer } from "@/components/layouts/Footer";
+// import { Footer } from "@/components/layouts/Footer";
 import { Navigation } from "@/components/layouts/Navbar";
 import { HerosSection } from "@/components/pages/home/Heros";
-import { HomeGallery } from "@/components/pages/home/HomeGallery";
+// import { HomeGallery } from "@/components/pages/home/HomeGallery";
 import { SummarySection } from "@/components/pages/home/Summary";
 
 export default function Home() {
@@ -10,8 +10,8 @@ export default function Home() {
       <Navigation gap="gap-14" />
       <HerosSection />
       <SummarySection />
-      <HomeGallery />
-      <Footer />
+      {/* <HomeGallery /> */}
+      {/* <Footer /> */}
     </>
   );
 }
